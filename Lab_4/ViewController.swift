e35602b03c70da35e21ef66cc68525f7ceb06c14
exemplerie <exemplerie@mail.ru>
2023-07-01 21:56:10 +0300

    Initial Commit

---
+++ Lab_4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab_4
+//
+//  Created by Валерия Харина on 01.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
